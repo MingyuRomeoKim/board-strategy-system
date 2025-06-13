8dced12d31b7708e8784bdaaefac98f9c837d404
mingyu <mingyu@mtn.co.kr>
2025-06-13 09:45:54 +0900

    #add, Post Entity 생성 및 H2 Database 관련 properties 작성

--- backend/src/main/java/com/mingyu/homework/api/v1/entity/Post.java
+++ backend/src/main/java/com/mingyu/homework/api/v1/entity/Post.java
@@ -1,17 +1,29 @@
 package com.mingyu.homework.api.v1.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Getter
 @Entity
+@Table(name = "posts")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "BINARY(16)")
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
     private UUID postId;
+
+    private String title;
+
+    private String content;
+
+    private String author;
+
+    private LocalDateTime createdAt;
 }
